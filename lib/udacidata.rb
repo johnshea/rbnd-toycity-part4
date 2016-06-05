c19c6200f33a6df04cb39e862bb8c14d63d1a9dd
John Shea <jscruffy@hotmail.com>
2016-06-04 23:10:00 -0400

    Finish implementing class Udacidata

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -2,40 +2,21 @@ require_relative 'find_by'
 require_relative 'errors'
 require 'csv'
 
-class Udacidata
+class Udacidata < Module
+  create_finder_methods :id, :brand, :name, :price
 
   def self.create(attributes = nil)
     @data_path = File.dirname(__FILE__) + "/../data/data.csv"
     db = CSV.read(@data_path)
 
-    # values = db.drop(1)
-    # item_in_db = false
-    # item_id = -1
-    # values.each do |id, brand, name, price|
-    #   if brand == attributes[:brand] && name == attributes[:name]
-    #     item_in_db = true
-    #     item_id = id
-    #   end
-    # end
-
-    # if !item_in_db
-    #   #p "write to db"
-      prod = Product.new(attributes)
-      #p @data_path
-      CSV.open(@data_path, 'wb') do |csv|
-        db.each do |data|
-          csv << data
-        end
-        csv << [prod.id, prod.brand, prod.name, prod.price]
+    prod = Product.new(attributes)
+    CSV.open(@data_path, 'wb') do |csv|
+      db.each do |data|
+        csv << data
       end
-    # else
-    #   attributes[:id] = item_id
-    #   prod = Product.new(attributes)
-    #   #p "exists in db"
-    # end
-    #p attributes
+      csv << [prod.id, prod.brand, prod.name, prod.price]
+    end
 
-   #p prod
     return prod
   end
 
@@ -53,30 +34,23 @@ class Udacidata
 
   def self.first(n = 1)
     data = self.all
-    # p data
-    # p n
-    # p data
-    # p data.first(n)[0]
+
     if n == 1
       return data.first(1)[0]
     else
       return data.first(n)
     end
-    # return data.first(n)[0]
   end
 
   def self.last(n = 1)
     data = self.all
-    # p data
-    # p n
-    # p data
-    # p data.first(n)[0]
+
     if n == 1
       return data.last(1)[0]
     else
       return data.last(n)
     end
-    # return data.first(n)[0]
+
   end
 
   def self.find(index)
@@ -107,5 +81,34 @@ class Udacidata
     end
 
     return deleted_product
+  end
+
+  def self.where(val)
+    items = Array.new
+    data = self.all
+    data.each do |item|
+      if val.keys[0] == :brand
+        if item.brand == val.values[0]
+          items << item
+        end
+      else
+        if item.name == val.values[0]
+          items << item
+        end
+      end
+    end
+    return items
+  end
+
+  def update(options={})
+    prod = Product.destroy(@id)
+
+    brand = options[:brand] ? options[:brand] : prod.brand
+    name = options[:name] ? options[:name] : prod.name
+    price = options[:price] ? options[:price] : prod.price
+
+    new_product = Product.create(id: prod.id, brand: brand, name: name, price: price)
+
+    return new_product
   end
 end
