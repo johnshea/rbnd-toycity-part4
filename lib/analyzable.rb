e496f44f72def389bd7da0a43a199126dfe4407d
John Shea <jscruffy@hotmail.com>
2016-06-05 19:50:36 -0400

    Refactor class Analyzable

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -13,46 +13,49 @@ module Analyzable
 
   def print_report(array_of_products)
     calculated_average_price = average_price(array_of_products)
-    brand_counts = count_by_brand(array_of_products)
-    name_counts = count_by_name(array_of_products)
 
     report_text = ""
     report_text += "Average Price: $#{calculated_average_price}\n"
-    report_text += "Inventory by Brand:\n"
-    brand_counts.each do |key, value|
-      report_text +=  "  - #{key}: #{value}\n"
-    end
-    report_text +=  "Inventory by Name:\n"
-    name_counts.each do |key, value|
-      report_text +=  "  - #{key}: #{value}\n"
-    end
+    report_text += format_report_section("brand", array_of_products)
+    report_text += format_report_section("name", array_of_products)
+
     return report_text
   end
 
-  def count_by_brand(array_of_products)
-
-    brand_counts = Hash.new
-    array_of_products.each do |product|
-      if brand_counts.has_key? product.brand
-        brand_counts[product.brand] += 1
-      else
-        brand_counts[product.brand] = 1
-      end
-    end
-    return brand_counts
+  def format_report_section(field, array_of_products)
+    counts = counts(field, array_of_products)
+    formatted_text = "Inventory by #{field.capitalize}:\n"
+    formatted_text += format_counts(counts)
+    return formatted_text
   end
 
+  def format_counts(counted_items)
+    formatted_text = ""
+    counted_items.each do |key, value|
+      formatted_text +=  "  - #{key}: #{value}\n"
+    end
+    return formatted_text
+  end
+
+  def count_by_brand(array_of_products)
+    counts = counts("brand", array_of_products)
+    return counts
+  end
 
   def count_by_name(array_of_products)
+    counts = counts("name", array_of_products)
+    return counts
+  end
 
-    name_counts = Hash.new
+  def counts(field, array_of_products)
+    item_counts = Hash.new
     array_of_products.each do |product|
-      if name_counts.has_key? product.name
-        name_counts[product.name] += 1
+      if item_counts.has_key? product.send(field)
+        item_counts[product.send(field)] += 1
       else
-        name_counts[product.name] = 1
+        item_counts[product.send(field)] = 1
       end
     end
-    return name_counts
+    return item_counts
   end
 end
